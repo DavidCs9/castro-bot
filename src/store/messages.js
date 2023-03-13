4d149de96288dbd7588ee336067a88383f7ec043
David Castro <davidcastrosi@hotmail.com>
2023-03-13 08:04:41 -0700

    fix api key

--- src/store/messages.js
+++ src/store/messages.js
@@ -35,7 +35,7 @@ export const useMessageStore = create((set, get) => ({
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${env.OPENAI_API_KEY}`
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
         },
         body: JSON.stringify({
           model: 'gpt-3.5-turbo',
