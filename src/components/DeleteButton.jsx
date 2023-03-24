82ab11b72b1af51e32c2a08937ae8c5371478074
DavidCs9 <davidcastrosi@hotmail.com>
2023-03-24 17:11:10 -0600

    fix delete btn

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -14,7 +14,7 @@ export function DeleteButton () {
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         onClick={handleDeleteChat} className='flex items-center px-6 py-2  transition ease-in duration-200 uppercase rounded-full
-       hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none'
+       hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none  mb-2'
       >
         <i class='fa-regular fa-trash-can pr-3' />
         Restart Chat
